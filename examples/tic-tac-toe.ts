86bc3f685f0ba9d4dab0d3a2fd95f30e218b82ea
Joseph Luck <joseph.luck@sky.com>
2017-10-26 15:21:03 +0100

    Smol

--- examples/tic-tac-toe.ts
+++ examples/tic-tac-toe.ts
@@ -5,58 +5,33 @@ type Square = '' | Player
 type Row = Square[]
 type Board = Row[]
 type Winner = Player | boolean
-
 type State = {
   board: Board
   turn: Player
   winner: Winner
 }
 
-const defaultState: State = {
-  board: emptyBoard(),
-  turn: 'x',
-  winner: false,
-}
-
-function getAllRows(board: Board): Square[][] {
-  return board
-}
-
-function getAllColumns(board: Board): Square[][] {
+function getAllColumns(board: Board): Board {
   return Array.from({ length: board.length })
-    .map((_, columnIndex) => {
-      return board.reduce((prev, row) => {
-        return [...prev, row[columnIndex]]
-      }, [])
-    })
+    .map((_, columnIndex) => board.reduce((prev, row) => {
+      return [...prev, row[columnIndex]]
+    }, [] as Square[]))
 }
 
 function isWon(vertice: Square[]): boolean {
   return vertice.every(square => square === 'x') || vertice.every(square => square === 'o')
 }
 
-function getWinningCombo(board: Board): Winner {
-  const combos = [...getAllRows(board), ...getAllColumns(board)]
+function determineWinner(board: Board): Winner {
+  const combos = [...board, ...getAllColumns(board)]
   return combos
     .reduce((prev, vertice) => {
-      return prev
-        ? prev
-        : isWon(vertice)
-          ? vertice[0] as Player
-          : false
+      return prev ? prev : isWon(vertice) ? vertice[0] as Player : false
     }, false as Winner)
 }
 
-function getWinner(board: Board): Winner {
-  const winner = getWinningCombo(board)
-  return winner ? winner : false
-}
-
-function emptyBoard(): Board {
-  return Array.from({ length: 3 })
-    .map(() => Array.from({ length: 3 })
-      .map(() => '' as Square)
-    )
+function createEmptyBoard(length): Board {
+  return Array.from({ length }).map(() => Array.from({ length }).map(() => '' as Square))
 }
 
 function updateBoard(state: State, clickedRowIndex: number, clickedColumnIndex: number): State {
@@ -65,11 +40,7 @@ function updateBoard(state: State, clickedRowIndex: number, clickedColumnIndex: 
     const isWherePlayerClicked = clickedRowIndex === rowIndex && clickedColumnIndex === columnIndex && square === ''
     return isWherePlayerClicked ? turn : square
   }))
-  return {
-    board,
-    turn,
-    winner: getWinner(board)
-  }
+  return state.winner ? state : { board, turn, winner: determineWinner(board) }
 }
 
 const renderSquare = (onclick: (rowIndex: number, columnIndex: number) => any, rowIndex: number) => (square: Square, columnIndex: number) => {
@@ -83,19 +54,15 @@ const renderRow = (onclick: (rowIndex: number, columnIndex: number) => any) => (
   return h('div', {}, row.map(renderSquare(onclick, rowIndex)))
 }
 
+const defaultState: State = {
+  board: createEmptyBoard(3),
+  turn: 'x',
+  winner: false,
+}
+
 const view: View<State> = (state = defaultState, update) => {
-  const onclick = (rowIndex: number, columnIndex: number) => {
-    if (!state.winner) {
-      update(updateBoard(state, rowIndex, columnIndex))
-    }
-  }
-  const renderWinner = () => {
-    if (state.winner) {
-      return `Player ${state.winner} won!`
-    } else {
-      return null
-    }
-  }
+  const onclick = (rowIndex: number, columnIndex: number) => update(updateBoard(state, rowIndex, columnIndex))
+  const renderWinner = () => state.winner ? `Player ${state.winner} won!` : null
   const renderBoard = () => h('div', {}, state.board.map(renderRow(onclick)))
   return h('div', {}, [renderBoard(), renderWinner()])
 }
